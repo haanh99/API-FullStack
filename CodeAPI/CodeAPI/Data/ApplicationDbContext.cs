f645fbb75d0f7c928db5d1fde4bd463352041cd6
haanh99 <haanh82867@gmail.com>
2025-04-20 16:13:41 +0700

    Add project files.

---
+++ CodeAPI/CodeAPI/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using CodeAPI.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace CodeAPI.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<BlogPost> BlogPots { get; set; }
+
+        public DbSet<Category> Categories { get; set; }
+    }
+}
