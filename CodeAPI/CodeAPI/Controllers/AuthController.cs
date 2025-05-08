e7b52f3fa486cc7449d0178b9f10c455dcd9352d
haanh99 <haanh82867@gmail.com>
2025-05-08 16:49:01 +0700

    Create Identity Table(Add-migration "Initial migration for Auth" -Context "AuthDbContext"--we are using 2 Db context so we need to specify that then Update-database -Context "AuthDbContext") Auth Controller and Register action method.

--- CodeAPI/CodeAPI/Controllers/AuthController.cs
+++ CodeAPI/CodeAPI/Controllers/AuthController.cs
@@ -1,63 +1,65 @@
-﻿//using CodeAPI.Models.DTO;
-//using Microsoft.AspNetCore.Http;
-//using Microsoft.AspNetCore.Identity;
-//using Microsoft.AspNetCore.Mvc;
+﻿using CodeAPI.Models.DTO;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 
-//namespace CodeAPI.Controllers
-//{
-//    [Route("api/[controller]")]
-//    [ApiController]
-//    public class AuthController : ControllerBase
-//    {
-//        private readonly UserManager<IdentityUser> _userManager;
+namespace CodeAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly UserManager<IdentityUser> _userManager;
 
-//        public AuthController(UserManager<IdentityUser> userManager)
-//        {
-//            this._userManager = userManager;
-//        }
-//        //POST: {apibaseUrl}/api/auth/register
-//        [HttpPost]
-//        [Route("register")]
-//        public async Task<IActionResult> Register([FromForm] RegisterRequestDto request)
-//        {
-//            //Create Identity object
-//            var user = new IdentityUser
-//            {
-//                UserName = request.Email?.Trim(),
-//                Email = request.Email?.Trim()
-//            };
-//            var identityResult = await _userManager.CreateAsync(user, request.Password);
+        public AuthController(UserManager<IdentityUser> userManager)
+        {
+            this._userManager = userManager;
+        }
+        //POST: {apibaseUrl}/api/auth/register
+        [HttpPost]
+        [Route("register")]
+        public async Task<IActionResult> Register([FromForm] RegisterRequestDto request)
+        {
+            //Create Identity object
+            var user = new IdentityUser
+            {
+                UserName = request.Email?.Trim(),
+                Email = request.Email?.Trim()
+            };
+            // Create User
+            var identityResult = await _userManager.CreateAsync(user, request.Password);
 
-//            if (identityResult.Succeeded)
-//            {
-//                // Add Role to user (reader)
-//                var addToResult = await _userManager.AddToRoleAsync(user, "Reader");
-//                if (addToResult.Succeeded)
-//                {
-//                    return Ok();
-//                }
-//                else
-//                {
-//                    if (addToResult.Errors.Any())
-//                    {
-//                        foreach (var error in addToResult.Errors)
-//                        {
-//                            ModelState.AddModelError("", error.Description);
-//                        }
-//                    }
-//                }
-                
-//            }
-//            else
-//            {
-//                if (identityResult.Errors.Any())
-//                {
-//                    foreach (var error in identityResult.Errors)
-//                    {
-//                        ModelState.AddModelError("", error.Description);
-//                    }
-//                }
-//            }
-//        }
-//    }
-//}
+            if (identityResult.Succeeded)
+            {
+                // Add Role to user (reader)
+                var addToResult = await _userManager.AddToRoleAsync(user, "Reader");
+                if (addToResult.Succeeded)
+                {
+                    return Ok();
+                }
+                else
+                {
+                    if (addToResult.Errors.Any())
+                    {
+                        foreach (var error in addToResult.Errors)
+                        {
+                            ModelState.AddModelError("", error.Description);
+                        }
+                    }
+                }
+
+            }
+            else
+            {
+                if (identityResult.Errors.Any())
+                {
+                    foreach (var error in identityResult.Errors)
+                    {
+                        ModelState.AddModelError("", error.Description);
+                    }
+                }
+            }
+            return ValidationProblem(ModelState);
+        }
+    }
+}
